48977d552bbdf84ebc85a6eb5358db78784fab68
T0wby <tobiasnuehlen@gmx.de>
2023-01-05 15:27:18 +0100

    -Added Manual Scores

--- RhythmGame/Assets/Scripts/Scriptables/Level/ScoreInfo.cs
+++ RhythmGame/Assets/Scripts/Scriptables/Level/ScoreInfo.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Scriptable
 {
-    [CreateAssetMenu(fileName = "LevelInfo", menuName = "Scriptable/Level/LevelInfo")]
+    [CreateAssetMenu(fileName = "NewScoreInfo", menuName = "Scriptable/Level/ScoreInfo")]
     public class ScoreInfo : ScriptableObject
     {
         [SerializeField] private int _placement;
@@ -12,5 +12,9 @@ namespace Scriptable
         [SerializeField] private float _accuracy;
         [SerializeField] private float _score;
 
+        public int Placement { get => _placement; set => _placement = value; }
+        public string PlayerName { get => _playerName; set => _playerName = value; }
+        public float Accuracy { get => _accuracy; set => _accuracy = value; }
+        public float Score { get => _score; set => _score = value; }
     }
 }
--- RhythmGame/Assets/Scripts/UI/LevelManager.cs
+++ RhythmGame/Assets/Scripts/UI/LevelManager.cs
@@ -11,35 +11,39 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private LevelInfo[] _levelCollection = null;
 
     [Header("Generated Objects")]
-    [SerializeField] GameObject _levelInformation;
-    [SerializeField] GameObject _scoreInformation;
-    [SerializeField] GameObject _levelParent;
-    [SerializeField] GameObject _scoreParent;
+    [SerializeField] private GameObject _levelInformation;
+    [SerializeField] private GameObject _scoreInformation;
+    [SerializeField] private GameObject _levelParent;
+    [SerializeField] private GameObject _scoreParent;
 
     [Header("LevelInfo Objects")]
-    [SerializeField] Button _easyButton;
-    [SerializeField] Button _normalButton;
-    [SerializeField] Button _hardButton;
+    [SerializeField] private Button _easyButton;
+    [SerializeField] private Button _normalButton;
+    [SerializeField] private Button _hardButton;
+    [SerializeField] private GameObject _emptyScore;
+    [SerializeField] private GameObject _filledScore;
+    private List<GameObject> _filledScoreChilds;
 
 
     private void Awake()
     {
+        _filledScoreChilds = new List<GameObject>();
         for (int i = 0; i < _levelCollection.Length; i++)
         {
+            LevelInfo levelInfo = _levelCollection[i];
             GameObject tmp = Instantiate(_levelInformation, _levelParent.transform);
             TMP_Text[] txtComponents = tmp.GetComponentsInChildren<TMP_Text>();
 
             //Setting values
-            txtComponents[0].text = _levelCollection[i].SongName;
-            txtComponents[1].text = _levelCollection[i].ArtistName;
-            txtComponents[2].text = _levelCollection[i].LevelDifficulty.ToString();
+            txtComponents[0].text = levelInfo.SongName;
+            txtComponents[1].text = levelInfo.ArtistName;
+            txtComponents[2].text = levelInfo.LevelDifficulty.ToString();
 
             //Setting OnClick Event
-
             Button levelButton = tmp.GetComponent<Button>();
             if (levelButton is not null)
             {
-                switch (_levelCollection[i].LevelDifficulty)
+                switch (levelInfo.LevelDifficulty)
                 {
                     case ELevelDifficulty.EASY:
                         levelButton.onClick.AddListener(() =>
@@ -68,11 +72,75 @@ public class LevelManager : MonoBehaviour
                     default:
                         break;
                 }
-
+                levelButton.onClick.AddListener(() =>
+                {
+                    LoadScores(levelInfo);
+                });
             }
 
             //Disable Button if Level not unlocked yet
             levelButton.interactable = _levelCollection[i].IsUnlocked;
         }
     }
+
+    #region Functions
+
+    /// <summary>
+    /// Loads Scores of a Level if it has any
+    /// </summary>
+    /// <param name="levelInfo">Level that the cores will be loaded from</param>
+    private void LoadScores(LevelInfo levelInfo)
+    {
+        List<ScoreInfo> scoreInfos = levelInfo.ScoreCollection;
+        int scoreCount = scoreInfos.Count;
+        if (scoreCount <= 0)
+        {
+            EmptyScores();
+        }
+        else
+        {
+            FillScores(scoreInfos, scoreCount);
+        }
+    }
+
+    /// <summary>
+    /// Empties the scores and sets the UI to empty
+    /// </summary>
+    private void EmptyScores()
+    {
+        _emptyScore.SetActive(true);
+        _filledScore.SetActive(false);
+
+        for (int i = 0; i < _filledScoreChilds.Count; i++)
+        {
+            Destroy(_filledScoreChilds[i]);
+        }
+        _filledScoreChilds.Clear();
+    }
+
+    /// <summary>
+    /// Adds all the available scores to the UI and activates it
+    /// </summary>
+    /// <param name="scoreInfos">List of scores that will be added</param>
+    /// <param name="scoreCount">Number of scores in the List</param>
+    private void FillScores(List<ScoreInfo> scoreInfos, int scoreCount)
+    {
+        for (int i = 0; i < scoreCount; i++)
+        {
+            GameObject tmp = Instantiate(_scoreInformation, _scoreParent.transform);
+            _filledScoreChilds.Add(tmp);
+            TMP_Text[] txtComponents = tmp.GetComponentsInChildren<TMP_Text>();
+
+            //Setting values
+            txtComponents[0].text = $"#{scoreInfos[i].Placement}";
+            txtComponents[1].text = scoreInfos[i].PlayerName;
+            txtComponents[2].text = $"{scoreInfos[i].Accuracy}%";
+            txtComponents[3].text = $"{scoreInfos[i].Score}";
+        }
+
+        _emptyScore.SetActive(false);
+        _filledScore.SetActive(true);
+    }
+
+    #endregion
 }
