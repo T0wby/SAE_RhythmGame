736392fe4e7c74fc700710bb2bcf423b32b34bdb
JKR <joerg.kreyer.mail@gmail.com>
2023-02-01 16:58:18 +0100

    Add Menu Garphic
    Funktionalität für Grafik und neues Farbschema übernommen

--- RhythmGame/Assets/Scripts/Menu/OptionSetting.cs
+++ RhythmGame/Assets/Scripts/Menu/OptionSetting.cs
@@ -2,11 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 public class OptionSetting : MonoBehaviour
 {
     public AudioMixer audioMaster;
     public AudioMixer audioMusic;
     public AudioMixer audioSFX;
+    public TMP_Dropdown resoulutionDropdown;
+    Resolution[] resolutions;
+
+    public void Start()
+    {
+        List<string> options = new List<string>();
+
+        resolutions = Screen.resolutions;
+        resoulutionDropdown.ClearOptions();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " X " + resolutions[i].height;
+            options.Add(option);
+        }
+        resoulutionDropdown.AddOptions(options);
+    }
     public void SetMasterVolume(float Volume) 
     {
         audioMaster.SetFloat("volumeMaster", Volume);
@@ -18,5 +37,13 @@ public class OptionSetting : MonoBehaviour
     public void SetSFXVolume(float Volume)
     {
         audioMaster.SetFloat("volumeSFX", Volume);
+    }
+    public void SetQuality(int qualityIndex) 
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
